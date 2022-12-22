ab4ec7bb453daf7f689e19d362e1d105cbddd2a5
Madhusankamsk <72614637+Madhusankamsk@users.noreply.github.com>
2022-12-22 17:00:28 +0530

    Add project files.

---
+++ Library_Management_System_1.0/Home.Designer.cs
@@ -0,0 +1,181 @@
+﻿namespace Library_Management_System_1._0
+{
+    partial class Home
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panelSide = new System.Windows.Forms.Panel();
+            this.buttonAboutUs = new System.Windows.Forms.Button();
+            this.buttonAuthorizing = new System.Windows.Forms.Button();
+            this.buttonRemoveBooks = new System.Windows.Forms.Button();
+            this.buttonMemberships = new System.Windows.Forms.Button();
+            this.buttonAddNewBooks = new System.Windows.Forms.Button();
+            this.buttonCategories = new System.Windows.Forms.Button();
+            this.buttonTotalBooks = new System.Windows.Forms.Button();
+            this.panelTop = new System.Windows.Forms.Panel();
+            this.panelBottom = new System.Windows.Forms.Panel();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panelSide.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panelSide
+            // 
+            this.panelSide.BackColor = System.Drawing.SystemColors.AppWorkspace;
+            this.panelSide.Controls.Add(this.buttonAboutUs);
+            this.panelSide.Controls.Add(this.buttonAuthorizing);
+            this.panelSide.Controls.Add(this.buttonRemoveBooks);
+            this.panelSide.Controls.Add(this.buttonMemberships);
+            this.panelSide.Controls.Add(this.buttonAddNewBooks);
+            this.panelSide.Controls.Add(this.buttonCategories);
+            this.panelSide.Controls.Add(this.buttonTotalBooks);
+            this.panelSide.Dock = System.Windows.Forms.DockStyle.Left;
+            this.panelSide.Location = new System.Drawing.Point(0, 0);
+            this.panelSide.Name = "panelSide";
+            this.panelSide.Size = new System.Drawing.Size(340, 740);
+            this.panelSide.TabIndex = 0;
+            // 
+            // buttonAboutUs
+            // 
+            this.buttonAboutUs.Location = new System.Drawing.Point(91, 436);
+            this.buttonAboutUs.Name = "buttonAboutUs";
+            this.buttonAboutUs.Size = new System.Drawing.Size(94, 29);
+            this.buttonAboutUs.TabIndex = 0;
+            this.buttonAboutUs.Text = "About Us";
+            this.buttonAboutUs.UseVisualStyleBackColor = true;
+            // 
+            // buttonAuthorizing
+            // 
+            this.buttonAuthorizing.Location = new System.Drawing.Point(91, 389);
+            this.buttonAuthorizing.Name = "buttonAuthorizing";
+            this.buttonAuthorizing.Size = new System.Drawing.Size(130, 29);
+            this.buttonAuthorizing.TabIndex = 0;
+            this.buttonAuthorizing.Text = "Authorizing";
+            this.buttonAuthorizing.UseVisualStyleBackColor = true;
+            // 
+            // buttonRemoveBooks
+            // 
+            this.buttonRemoveBooks.Location = new System.Drawing.Point(91, 341);
+            this.buttonRemoveBooks.Name = "buttonRemoveBooks";
+            this.buttonRemoveBooks.Size = new System.Drawing.Size(130, 29);
+            this.buttonRemoveBooks.TabIndex = 0;
+            this.buttonRemoveBooks.Text = "Remove books";
+            this.buttonRemoveBooks.UseVisualStyleBackColor = true;
+            // 
+            // buttonMemberships
+            // 
+            this.buttonMemberships.Location = new System.Drawing.Point(91, 294);
+            this.buttonMemberships.Name = "buttonMemberships";
+            this.buttonMemberships.Size = new System.Drawing.Size(130, 29);
+            this.buttonMemberships.TabIndex = 0;
+            this.buttonMemberships.Text = "Memberships";
+            this.buttonMemberships.UseVisualStyleBackColor = true;
+            // 
+            // buttonAddNewBooks
+            // 
+            this.buttonAddNewBooks.Location = new System.Drawing.Point(91, 247);
+            this.buttonAddNewBooks.Name = "buttonAddNewBooks";
+            this.buttonAddNewBooks.Size = new System.Drawing.Size(130, 29);
+            this.buttonAddNewBooks.TabIndex = 0;
+            this.buttonAddNewBooks.Text = "Add new books";
+            this.buttonAddNewBooks.UseVisualStyleBackColor = true;
+            // 
+            // buttonCategories
+            // 
+            this.buttonCategories.Location = new System.Drawing.Point(91, 200);
+            this.buttonCategories.Name = "buttonCategories";
+            this.buttonCategories.Size = new System.Drawing.Size(94, 29);
+            this.buttonCategories.TabIndex = 0;
+            this.buttonCategories.Text = "Categories";
+            this.buttonCategories.UseVisualStyleBackColor = true;
+            // 
+            // buttonTotalBooks
+            // 
+            this.buttonTotalBooks.Location = new System.Drawing.Point(91, 154);
+            this.buttonTotalBooks.Name = "buttonTotalBooks";
+            this.buttonTotalBooks.Size = new System.Drawing.Size(94, 29);
+            this.buttonTotalBooks.TabIndex = 0;
+            this.buttonTotalBooks.Text = "Total Books";
+            this.buttonTotalBooks.UseVisualStyleBackColor = true;
+            // 
+            // panelTop
+            // 
+            this.panelTop.BackColor = System.Drawing.SystemColors.Highlight;
+            this.panelTop.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panelTop.Location = new System.Drawing.Point(340, 0);
+            this.panelTop.Name = "panelTop";
+            this.panelTop.Size = new System.Drawing.Size(1011, 113);
+            this.panelTop.TabIndex = 1;
+            // 
+            // panelBottom
+            // 
+            this.panelBottom.BackColor = System.Drawing.Color.IndianRed;
+            this.panelBottom.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.panelBottom.Location = new System.Drawing.Point(340, 695);
+            this.panelBottom.Name = "panelBottom";
+            this.panelBottom.Size = new System.Drawing.Size(1011, 45);
+            this.panelBottom.TabIndex = 2;
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.Wheat;
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel1.Location = new System.Drawing.Point(340, 113);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(1011, 582);
+            this.panel1.TabIndex = 3;
+            // 
+            // Home
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1351, 740);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.panelBottom);
+            this.Controls.Add(this.panelTop);
+            this.Controls.Add(this.panelSide);
+            this.Name = "Home";
+            this.Text = "Form1";
+            this.panelSide.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private Panel panelSide;
+        private Panel panelTop;
+        private Panel panelBottom;
+        private Panel panel1;
+        private Button buttonAddNewBooks;
+        private Button buttonCategories;
+        private Button buttonTotalBooks;
+        private Button buttonRemoveBooks;
+        private Button buttonMemberships;
+        private Button buttonAboutUs;
+        private Button buttonAuthorizing;
+    }
+}
---
+++ Library_Management_System_1.0/Home.cs
@@ -0,0 +1,10 @@
+namespace Library_Management_System_1._0
+{
+    public partial class Home : Form
+    {
+        public Home()
+        {
+            InitializeComponent();
+        }
+    }
+}
